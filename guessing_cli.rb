057f71f2237283b0ec0296b7346c767b1833c758
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-11 20:59:42 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,5 +1,3 @@
-
-
 def run_guessing_game
   loop do
     number = rand(1..6).to_s
@@ -15,20 +13,3 @@ def run_guessing_game
     end
   end
 end
-
-
-
-=begin
-def run_guessing_game
-  number = rand(1..6).to_s
-  puts "Guess a number between 1 and 6."
-  user_guess = gets.chomp
-  if user_guess == "exit"
-    puts "Goodbye!"
-  elsif user_guess == number
-    puts "You guessed the correct number!"
-  else
-    puts "The computer guessed #{number}."
-  end
-end
-=end
